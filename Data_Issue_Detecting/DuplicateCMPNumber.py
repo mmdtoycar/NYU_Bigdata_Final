db3256263282487cb20fc83d4b7b7b59714dfafd
“qiming <qz718@nyu.edu>
2017-04-15 05:33:39 -0400

    revised DuplcateCheck

--- Data_Issue_Detecting/DuplicateCMPNumber.py
+++ Data_Issue_Detecting/DuplicateCMPNumber.py
@@ -18,12 +18,16 @@ if __name__ == "__main__":
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1], 1)
     lines = lines.mapPartitions(lambda x : reader(x))
-    counts = lines.map(lambda x: (x[0], 1)) \
-            .reduceByKey(add) \
-            .filter(lambda x : not(x[1] > 1) and not(ifIsNotValidNumberString(x[0]))) \
+    counts = lines.map(lambda x: (x[0], 1)) 
+    # store the internal result
+    element = counts
+    counts = counts.reduceByKey(add) \
+            .filter(lambda x : (x[1] > 1) or (ifIsNotValidNumberString(x[0]))) \
+            .map(lambda x: x[0]) \
+            .collect()
+    element = element.map(lambda x: (x[0], "INT\tComplaint Num\tValid") if x[0] not in counts
+                            else (x[0], "INT\tComplaint Num\tInvalid")) \
             .sortByKey() \
             .map(output)
-
-    counts.saveAsTextFile("DuplicateCMPNumber.out")
-
+    element.saveAsTextFile("DuplicateCMPNumber.out")
     sc.stop()
