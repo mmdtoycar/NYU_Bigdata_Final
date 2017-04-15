34265560ed0eeab53deb7d77775d9b5edef37ddf
“qiming <qz718@nyu.edu>
2017-04-15 04:38:01 -0400

    duplicateCMPCheck

--- Data_Issue_Detecting/DuplicateCMPNumber.py
+++ Data_Issue_Detecting/DuplicateCMPNumber.py
@@ -8,16 +8,8 @@ from csv import reader
 def output(Pair):
     return "%s\t%s" % (Pair[0], Pair[1])
 
-def process(Pair):
-    value = list(Pair[1])
-    value = len(value)
-    return (Pair[0], value)
-
 def ifIsNotValidNumberString(str):
     return not str.isdigit()
-
-def filt(x):
-    return (x[0], 1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -26,11 +18,12 @@ if __name__ == "__main__":
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1], 1)
     lines = lines.mapPartitions(lambda x : reader(x))
-    
-    counts = lines.map(filt)
+    counts = lines.map(lambda x: (x[0], 1)) \
+            .reduceByKey(add) \
+            .filter(lambda x : not(x[1] > 1) and not(ifIsNotValidNumberString(x[0]))) \
+            .sortByKey() \
+            .map(output)
 
-    counts = counts.groupByKey().filter(lambda x : len(list(x[1])) > 1 or ifIsNotValidNumberString(x[0])).map(process).sortByKey().map(output)
-
-    counts.saveAsTextFile("task.out")
+    counts.saveAsTextFile("DuplicateCMPNumber.out")
 
     sc.stop()
