ca60d83f047ed040af11f59dc347184c17d37311
chelseaisgood <chelseaisgoodfmd@gmail.com>
2017-04-15 14:06:03 -0400

    updates on CMPLNT_NUM.py

--- Data_Issue_Detecting/Basic_Issue/CMPLNT_NUM.py
+++ Data_Issue_Detecting/Basic_Issue/CMPLNT_NUM.py
@@ -5,23 +5,13 @@ import sys
 from operator import add
 from pyspark import SparkContext
 from csv import reader
+from helper.CHECK_BASE_TYPE import checkBaseType
 
 def output(Pair):
     return "%s\t%s" % (Pair[0], Pair[1])
 
 def ifIsNotValidNumberString(str):
     return not str.isdigit()
-
-def checkBaseType(x):
-    if x.isdigit():
-        return "INT"
-    elif unicode(x).isdecimal():
-        return "DECIMAL"
-    elif re.match(r"^(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/\d{4}$", x) \
-        or re.match(r"^(0?[1-9]|1[0-9]|2[0-3]):(0?[1-9]|[1-5][0-9]):(0?[1-9]|[1-5][0-9])$", x):
-        return "DATETIME"
-    else:
-        return "TEXT"
 
 def process(x):
     baseType = checkBaseType(x[0])
