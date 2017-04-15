4ba231c545c817eb201529169b4763c4a04ec008
“qiming <qz718@nyu.edu>
2017-04-15 12:36:40 -0400

    add

--- Data_Issue_Detecting/Basic_Issue/BRONX.py
+++ Data_Issue_Detecting/Basic_Issue/BRONX.py
@@ -1,16 +1,14 @@
 from __future__ import print_function
-
+import re
 import sys
 from operator import add
 from pyspark import SparkContext
 from csv import reader
-
 def output(Pair):
     return "%s\t%s" % (Pair[0], Pair[1])
 
 def ifIsNotValidNumberString(str):
     return not str.isdigit()
-
 def checkBaseType(x):
     if x.isdigit():
         return "INT"
@@ -21,37 +19,28 @@ def checkBaseType(x):
         return "DATETIME"
     else:
         return "TEXT"
-
 def process(x):
-    baseType = checkBaseType(x[0])
-    semanticType = "Complaint Num"
-    if x[0] == "":
-        return (x[0], "{}\t{}\tNULL".format(baseType, semanticType))
-    elif(x[0] not in counts):
-        return (x[0], "{}\t{}\tValid".format(baseType, semanticType))
+    baseType = checkBaseType(x)
+    semanticType = "Jurisdiction name"
+    if x == "":
+        return (x, "{}\t{}\tNULL".format(baseType, semanticType))
+    elif re.match(r"^.{5,30}$", x) :
+        return (x, "{}\t{}\tValid".format(baseType, semanticType))
     else :
-        return (x[0], "{}\t{}\tInvalid".format(baseType, semanticType))
-
+        return (x, "{}\t{}\tInvalid".format(baseType, semanticType))
+area = ("MANHATTAN", "")
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: task <file>", file=sys.stderr)
         exit(-1)
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1], 1)
-    #extract header
     header = lines.first()
     lines = lines.filter(lambda line: line != header)
-
     lines = lines.mapPartitions(lambda x : reader(x))
-    counts = lines.map(lambda x: (x[0].strip(), 1)) 
-    # store the internal result
-    element = counts
-    counts = counts.reduceByKey(add) \
-            .filter(lambda x : (ifIsNotValidNumberString(x[0]))) \
-            .map(lambda x: x[0]) \
-            .collect()
-    element = element.map(process) \
+    counts = lines.map(lambda x: x[13].strip()) \
+            .map(process) \
             .sortByKey() \
             .map(output)
-    element.coalesce(1).saveAsTextFile("CMPLNT_NUM.out")
+    counts.coalesce(1).saveAsTextFile("BRONX.out")
     sc.stop()
---
+++ Data_Issue_Detecting/Basic_Issue/N_Y_POLICE_DEPT.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import re
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+def output(Pair):
+    return "%s\t%s" % (Pair[0], Pair[1])
+
+def ifIsNotValidNumberString(str):
+    return not str.isdigit()
+def checkBaseType(x):
+    if x.isdigit():
+        return "INT"
+    elif unicode(x).isdecimal():
+        return "DECIMAL"
+    elif re.match(r"^(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/\d{4}$", x)
+        or re.match(r"^(0?[1-9]|1[0-9]|2[0-3]):(0?[1-9]|[1-5][0-9]):(0?[1-9]|[1-5][0-9])$", x):
+        return "DATETIME"
+    else:
+        return "TEXT"
+def process(x):
+    baseType = checkBaseType(x)
+    semanticType = "Jurisdiction name"
+    if x == "":
+        return (x, "{}\t{}\tNULL".format(baseType, semanticType))
+    elif re.match(r"^.{5,30}$", x) :
+        return (x, "{}\t{}\tValid".format(baseType, semanticType))
+    else :
+        return (x, "{}\t{}\tInvalid".format(baseType, semanticType))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: task <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    lines = sc.textFile(sys.argv[1], 1)
+    lines = lines.mapPartitions(lambda x : reader(x))
+    counts = lines.map(lambda x: (x[0].encode('utf-8').strip(), 1)) 
+    lines = lines.mapPartitions(lambda x : reader(x))
+    counts = lines.map(lambda x: x[12].strip()) \
+            .map(process) \
+            .sortByKey() \
+            .map(output)
+    counts.coalesce(1).saveAsTextFile("N_Y_POLICE_DEPT.out")
+    sc.stop()
