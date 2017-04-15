a075209ad8285f45bb61062e14f32fc98afff1f7
chelseaisgood <chelseaisgoodfmd@gmail.com>
2017-04-15 12:26:55 -0400

    duplicate CMP number checking

--- Data_Issue_Detecting/Other_Issue/DuplicateCMPNumber.py
+++ Data_Issue_Detecting/Other_Issue/DuplicateCMPNumber.py
@@ -8,41 +8,26 @@ from csv import reader
 def output(Pair):
     return "%s\t%s" % (Pair[0], Pair[1])
 
-def ifIsNotValidNumberString(str):
-    return not str.isdigit()
-
-def checkBaseType(x):
-    if x.isdigit():
-        return "INT"
-    elif x.isdecimal():
-        return "DECIMAL"
-    else:
-        return "TEXT"
-
 def process(x):
-    baseType = checkBaseType(x[0])
-    semanticType = "Complaint Num"
-    if(x[0] not in counts):
-        return (x[0], "{}\t{}\tValid".format(baseType, semanticType))
-    else :
-        return (x[0], "{}\t{}\tInvalid".format(baseType, semanticType))
-
+    invalidType = "Has_Duplicate_Confict_Value"
+    return (x[0], "AppearTime: {}\t{}".format(x[1], invalidType))
+    
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: task <file>", file=sys.stderr)
         exit(-1)
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1], 1)
+    #extract header
+    header = lines.first()
+    lines = lines.filter(lambda line: line != header)
+
     lines = lines.mapPartitions(lambda x : reader(x))
-    counts = lines.map(lambda x: (x[0], 1)) 
-    # store the internal result
-    element = counts
+    counts = lines.map(lambda x: (x[0].strip(), 1)) 
     counts = counts.reduceByKey(add) \
-            .filter(lambda x : (x[1] > 1) or (ifIsNotValidNumberString(x[0]))) \
-            .map(lambda x: x[0]) \
-            .collect()
-    element = element.map(process) \
+            .filter(lambda x : (x[1] > 1)) \
+            .map(process) \
             .sortByKey() \
             .map(output)
-    element.saveAsTextFile("DuplicateCMPNumber.out")
+    counts.saveAsTextFile("DuplicateCMPNumber.out")
     sc.stop()
